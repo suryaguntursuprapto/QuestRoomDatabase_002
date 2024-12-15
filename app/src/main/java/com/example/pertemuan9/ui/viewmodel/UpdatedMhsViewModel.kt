c8371c34e86f32ac42e432ae08b81c220b0469e3
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-12-15 17:40:46 +0700

    Tambahkan UpdateMhsViewModel untuk Mengelola Pembaruan Mahasiswa

---
+++ app/src/main/java/com/example/pertemuan9/ui/viewmodel/UpdatedMhsViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.pertemuan9.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan9.data.entity.Mahasiswa
+import com.example.pertemuan9.repository.RepositoryMhs
+import com.example.pertemuan9.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUIState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMh()
+        }
+    }
+
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        updateUIState = updateUIState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
+
+}
